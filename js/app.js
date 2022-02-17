010fb18b0bd0b9449836704b177e6ce7feab4d0f
Krystian888-lab <krystianmarciniak446@gmail.com>
2022-02-18 00:33:20 +0100

    compute set style

--- js/app.js
+++ js/app.js
@@ -42,18 +42,17 @@ Calculator.prototype.addEvents = function () {
 }
 
 //7.Aktualizacja sumy
-
-Calculator.prototype.updateTotal = function () {
+Calculator.prototype.updateTotal = function() {
  const show = this.summary.list.querySelectorAll( ".open" ).length > 0;
 
  if ( show ) {
   const productsSum = this.form.products.value < 0
   ? 0
-  : this.prices.products * this.form.products.value;
+  : this.form.products.value * this.prices.products;
 
   const ordersSum = this.form.orders.value < 0
   ? 0
-  : this.prices.orders * this.form.orders.value;
+  : this.form.orders.value * this.prices.orders;
 
   const packagePrice = this.form.package.dataset.value.length === 0
   ? 0
@@ -67,7 +66,7 @@ Calculator.prototype.updateTotal = function () {
   ? this.prices.terminal
   : 0;
 
-  this.summary.total.price.innerText = `\$${productsSum + ordersSum + packagePrice + accounting + terminal}`;
+  this.summary.total.price.innerText = `\$${ productsSum + ordersSum + packagePrice + accounting + terminal }`;
 
   this.summary.total.container.classList.add( "open" );
  } else {
@@ -76,83 +75,83 @@ Calculator.prototype.updateTotal = function () {
 }
 
 //3. Aktualizacja
-Calculator.prototype.updateSummary = function (id, calc, total, callback) {
- const summary = this.summary.list.querySelector(`[data-id=${id}]`);
- const summaryCalc = summary.querySelector(".t-file__multi");
- const summaryTotal = summary.querySelector(".t-file__price");
+Calculator.prototype.updateSummary = function ( id, calc, total, callback ) {
+ const summary = this.summary.list.querySelector( "[data-id="+id+"]" );
+ const summaryCalc = summary.querySelector( ".multi" );
+ const summaryTotal = summary.querySelector( ".price" );
 
- summary.classList.add("open");
+ summary.classList.add( "open" );
 
- if (summaryCalc !== null) {
+ if ( summaryCalc !== null ) {
   summaryCalc.innerText = calc;
- };
+ }
 
- summaryTotal.innerText = `\$${total}`;
+ summaryTotal.innerText = `\$${ total }`;
 
- if (typeof callback === "function") {
-  callback(summary, summaryCalc, summaryTotal);
- };
+ if ( typeof callback === "function" ) {
+  callback( summary, summaryCalc, summaryTotal )
+ }
 }
 
-
 //2. Obsługa pól tekstowych
-Calculator.prototype.inputEvent = function (el) {
+Calculator.prototype.inputEvent = function ( el ) {
  const id = el.currentTarget.id;
  const value = el.currentTarget.value;
- const singlePrice = this.prices[id];
+ const singlePrice = this.prices[ id ];
  const totalPrice = value * singlePrice;
 
- this.updateSummary(id, `${value} *\$${singlePrice}`, totalPrice, function (item, calc, total) {
-  if (value < 0) {
+ this.updateSummary( id, `${ value }*\$${ singlePrice }`, totalPrice, function( item, calc, total ) {
+  if ( value < 0 ) {
    calc.innerHTML = null;
    total.innerText = "Value should be greater than 0";
-  }
+  };
 
-  if (value.length === 0) {
-   item.classList.remove("open");
+  if ( value.length === 0 ) {
+   item.classList.remove( "open" );
   }
- });
+ } );
  this.updateTotal();
 }
 
 
 //4. Obsługa niestandardowego selecta
-Calculator.prototype.selectEvent = function (el) {
- this.form.package.classList.toggle("open");
+Calculator.prototype.selectEvent = function ( el ) {
+ this.form.package.classList.toggle( "open" );
 
- const value = typeof el.target.dataset.value !== "undefined" ?
-  el.target.dataset.value :
-  "";
+ const value = typeof el.target.dataset.value !== "undefined"
+ ? el.target.dataset.value
+ : "";
 
- const text = typeof el.target.dataset.text !== "undefined" ?
-  el.target.innerText :
-  "Choose package";
+ const text = typeof el.target.dataset.text !== "undefined"
+ ? el.target.innerText
+ : "Choose package";
 
- if (value.length > 0) {
+ if ( value.length > 0 ) {
   this.form.package.dataset.value = value;
-  this.form.package.querySelector(".select__input").innerText = text;
-  this.updateSummary("package", text, this.prices.package[value]);
+  this.form.package.querySelector( ".select" ).innerText = text;
+  this.updateSummary( "package", text, this.prices.package[ value ] );
   this.updateTotal();
- }
+ };
 }
 
 //5. Obsługa checkboxow
-Calculator.prototype.checkboxEvent = function (el) {
+Calculator.prototype.checkboxEvent = function ( el ) {
  const checkbox = el.currentTarget;
  const id = checkbox.id;
  const checked = el.currentTarget.checked;
 
- this.updateSummary(id, undefined, this.prices[id], function (item) {
-  if (!checked) {
-   item.classList.remove("open")
+ this.updateSummary( id, undefined, this.prices[ id ], function ( item ) {
+  if ( !checked ) {
+   item.classList.remove( "open" );
   }
- });
+ } );
+
  this.updateTotal();
-}
+};
 
 // Wywołanie
 document.addEventListener("DOMContentLoaded", function () {
- const form = document.querySelector(".calc__form");
+ const form = document.querySelector(".c-com__form");
  const summary = document.querySelector(".t-com__compute");
 
  new Calculator(form, summary);
